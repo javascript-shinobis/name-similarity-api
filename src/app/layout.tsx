f6e16c024a7b357347ef32645bc84978fe8f5da7
Abhishek Mishra <abhisk.mhra@gmail.com>
2023-05-07 20:07:23 +0530

    style: setup basic styling for `RootLayout` component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
+import { Inter } from "next/font/google";
+
 import "@/styles/globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -6,8 +11,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn("bg-white text-slate-900 antialiased", inter.className)}
+    >
+      <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
+        {children}
+      </body>
     </html>
   );
 }
