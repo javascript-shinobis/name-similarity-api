ea38caae130d26509ff10562d16e1e77afdc86d3
Abhishek Mishra <abhisk.mhra@gmail.com>
2023-05-07 20:24:31 +0530

    feat: import `Provider` component to use in `RootLayout`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 
 import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
+import Provider from "@/components/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
       className={cn("bg-white text-slate-900 antialiased", inter.className)}
     >
       <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
-        {children}
+        <Provider>{children}</Provider>
         {/* Allow more height on mobile devices */}
         <div className="h-40 md:hidden" />
       </body>
